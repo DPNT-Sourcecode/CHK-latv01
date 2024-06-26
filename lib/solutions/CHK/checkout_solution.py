4712d3bca4b65f164855b4301c23ff8e5a15d02c
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2024-06-26 21:49:21 +0100

    Wed Jun 26 21:49:21 BST 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,3 +1,4 @@
+from Collections import Counter
 
 class Offer:
     def __init__(self, item, price, special):
@@ -6,22 +7,35 @@ class Offer:
         self.special = special
 
 
+class Offers:
+    def __init__(self):
+        self.items = dict()
 
-Our price table and offers:
-+------+-------+----------------+
-| Item | Price | Special offers |
-+------+-------+----------------+
-| A    | 50    | 3A for 130     |
-    | B    | 30    | 2B for 45      |
-    | C    | 20    |                |
-| D    | 15    |                |
-+------+-------+----------------+
+    def add_item(self, item: Offer):
+        self.items[item.item] = item
+
+    def specials(self, item):
+        return self.items[item]
+
+    def check_special(self):
+        return 0
+
 # noinspection PyUnusedLocal
 # skus = unicode string
+
+offers = Offers()
+offers.add_item(Offer("A", 50, "3A for 130"))
+offers.add_item(Offer("B", 30, "2B for 45"))
+offers.add_item(Offer("C", 20, ""))
+offers.add_item(Offer("D", 20, ""))
+
+
 def checkout(skus):
-    print(skus)
+    print(skus, offers, Counter(skus))
+
 
 checkout("AAAAABBBCCAAA")
+
 
 
 
