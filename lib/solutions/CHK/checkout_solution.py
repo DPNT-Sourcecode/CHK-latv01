24abc6a7cc5ec78ac45087f795468b961e3a1907
Julian Ghionoiu <iulian.ghionoiu@gmail.com>
2024-06-26 21:43:21 +0100

    Wed Jun 26 21:43:21 BST 2024

--- lib/solutions/CHK/checkout_solution.py
+++ lib/solutions/CHK/checkout_solution.py
@@ -1,9 +1,27 @@
 
+class Offer:
+    def __init__(self, item, price, special):
+        self.item = item
+        self.price = price
+        self.special = special
+
+
+
+Our price table and offers:
++------+-------+----------------+
+| Item | Price | Special offers |
++------+-------+----------------+
+| A    | 50    | 3A for 130     |
+    | B    | 30    | 2B for 45      |
+    | C    | 20    |                |
+| D    | 15    |                |
++------+-------+----------------+
 # noinspection PyUnusedLocal
 # skus = unicode string
 def checkout(skus):
     print(skus)
 
 checkout("AAAAABBBCCAAA")
+
 
 
